7c51cbbbeb1b2a90fb6237e9d9f457e39af48423
Uver Diaz <front.desarrollo@ideascontrol.com>
2024-09-08 07:08:23 -0500

    feat: Create and implement UI screens with responsive design

---
+++ lib/core/widgets/comic_tile.dart
@@ -0,0 +1,60 @@
+import 'package:comic_vine_app/core/contracts/i_size_config.dart';
+import 'package:comic_vine_app/core/widgets/custom_divider.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:comic_vine_app/core/contracts/i_date_formatter.dart';
+import 'package:comic_vine_app/core/contracts/i_theme_config.dart';
+import 'package:comic_vine_app/core/theme/app_theme.dart';
+
+class ComicTile extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String issueNumber;
+  final DateTime releaseDate;
+
+  final IDateFormatter dateFormatter = GetIt.I<IDateFormatter>();
+  final themeConfig = GetIt.I<IThemeConfig>() as AppTheme;  // Obtain theme configuration
+  final responsiveConfig = GetIt.I<ISizeConfig>();  // Obtain responsive configuration
+
+  ComicTile({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.issueNumber,
+    required this.releaseDate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),  // Uniform padding
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,  // Align text to the left
+        children: [
+          // Display comic image with full width
+          Image.network(imageUrl, height: responsiveConfig.getHeightSize(70), width: double.infinity, fit: BoxFit.cover),
+          SizedBox(height: responsiveConfig.getHeightSize(2.5)),
+          Text(
+            '$title #$issueNumber',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 19,
+              color: themeConfig.getDefaultTextColor(),  // Use theme for title color
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: responsiveConfig.getHeightSize(0.6)),
+          Text(
+            dateFormatter.formatDateWithMonthName(releaseDate),  // Using the injected date formatter for the release date
+            style: TextStyle(
+              fontSize: 16,
+              color: themeConfig.getSubtitleSectionColor(),  // Use theme for default text color
+            ),
+          ),
+          SizedBox(height: responsiveConfig.getHeightSize(1)),
+          CustomDivider(),
+        ],
+      ),
+    );
+  }
+}
